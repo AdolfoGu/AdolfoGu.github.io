eaf83208d1d33b68fe24f2436ac8d5e6cc40bf57
Adolfo Gutierrez <ja.gutierrez.gaytan@ugto.mx>
2023-10-17 08:34:07 -0600

    Nuevo estilo

--- resources/js/app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("submit").addEventListener('click',function(){
-    var nom = $('#nombre').val();
-    var cor = $('#correo').val();
-    var tel = $('#telefono').val();
-    var com = $('#comen').val();
-
-    alert(nom+" "+cor+" "+tel+" "+com);
-})
